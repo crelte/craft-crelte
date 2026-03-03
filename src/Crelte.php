ceac5f4b3b12dd686f53e1b0ac734a62e8fbf50b
Sören Meier <soeren@s-me.ch>
2026-03-03 22:33:08 +0100

    improve when the queries cache should be cleared & add an option to the clear caches page

--- src/Crelte.php
+++ src/Crelte.php
@@ -8,18 +8,29 @@ use craft\helpers\App;
 use yii\base\Event;
 use craft\base\Model;
 use craft\base\Plugin;
+use craft\events\RegisterCacheOptionsEvent;
 use craft\events\RegisterGqlQueriesEvent;
 use craft\events\RegisterGqlTypesEvent;
 use craft\events\RegisterGqlSchemaComponentsEvent;
 use craft\events\RegisterCpAlertsEvent;
 use craft\services\Gql;
 use craft\gql\GqlEntityRegistry;
-use craft\helpers\{Cp, Html, UrlHelper};
+use craft\helpers\{Cp, ElementHelper, Html, UrlHelper};
+use craft\utilities\ClearCaches;
 use craft\web\Application;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use crelte\crelte\models\Settings;
 use crelte\crelte\gql\queries\{SitesQuery, StarterQuery};
+
+// These are a list of classnames which should be ignored,
+// because they are not important for the queries cache
+const IGNORE_ELEMENT_SAVE = [
+	'craft\elements\User',
+	'verbb\formie\elements\Submission',
+	'craft\commerce\elements\Order',
+	'craft\elements\Address'
+];
 
 /**
  * craft-crelte plugin
@@ -33,15 +44,15 @@ class Crelte extends Plugin
 
 	private bool $webhookRequested = false;
 
-	const EDITION_SOLO = "solo";
-	const EDITION_PRO = "pro";
+	public const EDITION_SOLO = "solo";
+	public const EDITION_PRO = "pro";
 
 	public static function editions(): array
 	{
 		return [self::EDITION_SOLO, self::EDITION_PRO];
 	}
 
-	public function init()
+	public function init(): void
 	{
 		parent::init();
 
@@ -75,11 +86,11 @@ class Crelte extends Plugin
 		Event::on(Gql::class, Gql::EVENT_REGISTER_GQL_QUERIES, function (
 			RegisterGqlQueriesEvent $event
 		) use ($inDevMode) {
-			$event->queries = array_merge(
-				$event->queries,
-				SitesQuery::getQueries(),
-				$inDevMode ? StarterQuery::getQueries() : []
-			);
+			$event->queries = [
+				...$event->queries,
+				...SitesQuery::getQueries(),
+				...($inDevMode ? StarterQuery::getQueries() : [])
+			];
 		});
 
 		Event::on(
@@ -113,7 +124,7 @@ class Crelte extends Plugin
 					Html::beginTag("p", [
 						"class" => ["flex", "flex-nowrap"],
 					]) .
-					sprintf(
+					\sprintf(
 						'<a class="go" href="%s">%s</a>',
 						UrlHelper::cpUrl("plugin-store/craft-crelte"),
 						Craft::t("app", "Resolve now")
@@ -124,6 +135,7 @@ class Crelte extends Plugin
 		});
 
 		$enableCaching = App::env("CACHING") === true;
+		// for local dev CRAFT_FRONTEND_URL should be http://host.docker.internal:8080
 		$frontendUrl = App::env("CRAFT_FRONTEND_URL") ?: App::env("FRONTEND_URL") ?: null;
 		$token = App::env("ENDPOINT_TOKEN") ?: null;
 		if ($enableCaching && $frontendUrl && $token) {
@@ -135,10 +147,40 @@ class Crelte extends Plugin
 		string $frontendUrl,
 		string $token
 	): void {
+		// Add a new "clear cache" checkbox option
+		Event::on(
+			ClearCaches::class,
+			ClearCaches::EVENT_REGISTER_CACHE_OPTIONS,
+			function (RegisterCacheOptionsEvent $event) {
+				$event->options[] = [
+					'key' => 'crelte-queries-cache',
+					'label' => 'Crelte queries cache',
+					'info' => 'Clears crelte queries cache',
+					'action' => function () {
+						$this->webhookRequested = true;
+					},
+				];
+			}
+		);
+
+
 		// Listen for element save events
 		Event::on(Element::class, Element::EVENT_AFTER_SAVE, function (
 			Event $event
 		) {
+			/** @var Element $element */
+			$element = $event->sender;
+
+			if (ElementHelper::isDraftOrRevision($element)) {
+				return;
+			}
+
+			$className = \get_class($element);
+			if (\in_array($className, IGNORE_ELEMENT_SAVE, true)) {
+				return;
+			}
+			// Craft::info("event after save " . $className);
+
 			// for this to work correctly you need to setup like
 			// https://github.com/craftcms/cms/pull/17024
 			// */5 * * * *
@@ -189,6 +231,8 @@ class Crelte extends Plugin
 						$response->getStatusCode(),
 					__METHOD__
 				);
+			} else {
+				Craft::info("crelte queries cache webhook called");
 			}
 		} catch (RequestException $e) {
 			Craft::error(
